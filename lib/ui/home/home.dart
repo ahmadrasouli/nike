ade337ffd7c4e2fdeba918dc28518f467c55eb3f
ahmadrasouli <rasouli.ahmad90@gmail.com>
2024-01-16 10:03:47 +0330

    3333

--- lib/ui/home/home.dart
+++ lib/ui/home/home.dart
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-
-=======
->>>>>>> 5df5f4323dbf1fb8e502bb79cfbeb587ed997842
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:nike2/data/product.dart';
